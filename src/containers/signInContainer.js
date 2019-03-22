6498a22151e987b50473489a6cc47961b39cd2f0
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-22 13:00:10 +0200

    modified signIn container

--- src/containers/signInContainer.js
+++ src/containers/signInContainer.js
@@ -1,3 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import SignIn from '../components/SignIn';
+import { changeSignInLogin, changeSignInPassword } from '../actions/signInActions';
+
+const mapStateToProps = state => ({
+    login: state.signIn.signInLogin,
+    password: state.signIn.signInPassword
+});
+
+const mapDispatchToProps = dispatch => ({
+    changeLogin: bindActionCreators(changeSignInLogin, dispatch),
+    changePassword: bindActionCreators(changeSignInPassword, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
 
 
