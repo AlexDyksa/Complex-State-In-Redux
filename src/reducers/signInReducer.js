102c26a1e2f02511a472e66fec18c2ebc0a18ca1
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-22 12:42:09 +0200

    modified signin reducer

---
+++ src/reducers/signInReducer.js
@@ -0,0 +1,24 @@
+import { SIGNIN_CHANGE_LOGIN, SIGNIN_CHANGE_PASSWORD } from '../actions/types';
+
+const initialState = {
+    signInLogin: '',
+    signInPassword: ''
+};
+
+export const signInReducer = (state = initialState, action) => {
+    console.log('sss');
+    switch (action.type) {
+        case SIGNIN_CHANGE_LOGIN:
+            return {
+                ...state,
+                signInLogin: action.payload
+            };
+        case SIGNIN_CHANGE_PASSWORD:
+            return {
+                ...state,
+                signInPassword: action.payload
+            };
+        default:
+            return state;
+    }
+};
