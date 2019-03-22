fda3ad1270e53e673629a82c0fa7c12fd56c4b3b
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-22 11:24:49 +0200

    modified and styled component SignIn

--- src/components/signIn/index.js
+++ src/components/signIn/index.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import './styles.css'
 
 class SignIn extends Component {
+    handleSubmit = e => e.preventDefault();
+
     render() {
         return (
-            <form className="sign-in">
-                <input type="text" className="sign-in__login" />
-                <input type="password" className="sign-in__password" />
+            <form
+                className="sign-in"
+                onSubmit={this.handleSubmit}
+            >
+                <input
+                    type="text"
+                    className="sign-in__login"
+                    placeholder="login"
+                    value=''
+                />
+                <input
+                    type="password"
+                    className="sign-in__password"
+                    placeholder="password"
+                    value=''
+                />
+                <input
+                    type="submit"
+                    className="sing-in__submit"
+                    value="Sign In"
+                />
             </form>
         );
     }
