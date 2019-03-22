6300377fac7c30b69379ba986a42a3994f60c482
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-22 11:06:53 +0200

    modified export default of store

--- src/store.js
+++ src/store.js
@@ -2,4 +2,6 @@ import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
-export const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
