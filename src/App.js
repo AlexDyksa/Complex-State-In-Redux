960d5d863fd2048cfbfdd56f17b16e92ae885fc1
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-22 10:33:35 +0200

    add markup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import SignIn from './containers/signInContainer';
+import SignUp from './containers/signUpContainer';
 
-class App extends React.Component {
+class App extends Component {
     render() {
         return (
             <div className="App">
-
+                <div className="app__inner">
+                    <div className="app__row">
+                        <div className="app__col">
+                            <SignIn />
+                        </div>
+                        <div className="app__col">
+                            <SignUp />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
