d4b03887221c2c92ccbd6e7e7ad53defeac5baaa
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-25 10:38:53 +0200

    added onClick method for submit button

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -4,53 +4,61 @@ import './styles.css'
 class SignUp extends Component {
     handleSubmit = e => {
         e.preventDefault();
-    }
+    };
 
     handleChangeLogin = e => {
         this.props.changeLogin(e.target.value);
-    }
+    };
 
     handleChangePassword = e => {
         this.props.changePassword(e.target.value);
-    }
+    };
 
     handleChangeRepeatPassword = e => {
         this.props.changeRepeatPassword(e.target.value);
-    }
+    };
+
+    handleClickBtn = () => {
+        alert('You\'ve Signed Up');
+    };
 
     render() {
         return (
-            <form
-                className="sign-up"
-                onSubmit={this.handleSubmit}
-            >
-                <input
-                    type="text"
-                    className="sign-up__login"
-                    placeholder="login"
-                    value={this.props.login}
-                    onChange={this.handleChangeLogin}
-                />
-                <input
-                    type="password"
-                    className="sign-up__password"
-                    placeholder="password"
-                    value={this.props.password}
-                    onChange={this.handleChangePassword}
-                />
-                <input
-                    type="password"
-                    className="sign-up__repeat-password"
-                    placeholder="repeat password"
-                    value={this.props.repeatPassword}
-                    onChange={this.handleChangeRepeatPassword}
-                />
-                <input
-                    type="submit"
-                    className="sing-up__submit"
-                    value="Sign Up"
-                />
-            </form>
+            <React.Fragment>
+                <h2 className="component-title">Component #2</h2>
+                <form
+                    className="sign-up"
+                    onSubmit={this.handleSubmit}
+                >
+                    <input
+                        type="text"
+                        className="sign-up__login"
+                        placeholder="login"
+                        value={this.props.login}
+                        onChange={this.handleChangeLogin}
+                    />
+                    <input
+                        type="password"
+                        className="sign-up__password"
+                        placeholder="password"
+                        value={this.props.password}
+                        onChange={this.handleChangePassword}
+                    />
+                    <input
+                        type="password"
+                        className="sign-up__repeat-password"
+                        placeholder="repeat password"
+                        value={this.props.repeatPassword}
+                        onChange={this.handleChangeRepeatPassword}
+                    />
+                    <input
+                        type="submit"
+                        className="sing-up__submit"
+                        value="Sign Up"
+                        onClick={this.handleClickBtn}
+                    />
+                </form>
+            </React.Fragment>
         );
     }
 }
--- src/components/signIn/index.js
+++ src/components/signIn/index.js
@@ -4,42 +4,50 @@ import './styles.css'
 class SignIn extends Component {
     handleSubmit = e => {
         e.preventDefault();
-    }
+    };
 
     handleChangeLogin = e => {
         this.props.changeLogin(e.target.value);
-    }
+    };
 
     handleChangePassword = e => {
         this.props.changePassword(e.target.value);
-    }
+    };
+
+    handleClickBtn = () => {
+        alert('You\'ve Signed In');
+    };
 
     render() {
         return (
-            <form
-                className="sign-in"
-                onSubmit={this.handleSubmit}
-            >
-                <input
-                    type="text"
-                    className="sign-in__login"
-                    placeholder="login"
-                    value={this.props.login}
-                    onChange={this.handleChangeLogin}
-                />
-                <input
-                    type="password"
-                    className="sign-in__password"
-                    placeholder="password"
-                    value={this.props.password}
-                    onChange={this.handleChangePassword}
-                />
-                <input
-                    type="submit"
-                    className="sing-in__submit"
-                    value="Sign In"
-                />
-            </form>
+            <React.Fragment>
+                <h2 className="component-title">Component #1</h2>
+                <form
+                    className="sign-in"
+                    onSubmit={this.handleSubmit}
+                >
+                    <input
+                        type="text"
+                        className="sign-in__login"
+                        placeholder="login"
+                        value={this.props.login}
+                        onChange={this.handleChangeLogin}
+                    />
+                    <input
+                        type="password"
+                        className="sign-in__password"
+                        placeholder="password"
+                        value={this.props.password}
+                        onChange={this.handleChangePassword}
+                    />
+                    <input
+                        type="submit"
+                        className="sing-in__submit"
+                        value="Sign In"
+                        onClick={this.handleClickBtn}
+                    />
+                </form>
+            </React.Fragment>
         );
     }
 }
